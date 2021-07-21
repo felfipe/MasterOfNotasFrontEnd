0d3055b082fe5d4e55e4aac60364a374724da751
Mateus Fernandes Doimo <mateusdoimo@usp.br>
2021-07-21 00:46:43 -0300

    update quiz aluno

--- src/modules/Quiz/QuizView.vue
+++ src/modules/Quiz/QuizView.vue
@@ -1,26 +1,134 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
+    <MenuQuiz :controle="drawer" :num_question='10'></MenuQuiz>
+    <v-app-bar
+      color="blue darken-2" 
+      absolute
     >
-      <!--  -->
-    </v-navigation-drawer>
-
-    <v-app-bar app>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-app-bar-nav-icon color='white' @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-img
+        :src="require('@/assets/logo-w.png')"
+        max-height="30"
+        max-width="200"
+      ></v-img>
     </v-app-bar>
 
-    <v-main>
-      <!--  -->
+    <v-main >
+      <v-container>
+        <v-breadcrumbs class="breadcrumbs-quiz" :items="items"></v-breadcrumbs>
+        <!-- <QuestQuiz></QuestQuiz> -->
+        <router-view></router-view>
+        <div class="btns-nav-quiz">
+          <v-btn
+            depressed
+            color="primary"
+            x-large
+            disabled
+          >
+            <v-icon dark large>
+              mdi-chevron-left
+            </v-icon>  
+          </v-btn>
+          <v-btn
+            depressed
+            color="primary"
+            x-large
+          >
+            <v-icon dark large>
+              mdi-chevron-right
+            </v-icon>  
+          </v-btn>
+        </div>
+        
+      </v-container>
     </v-main>
+
+    <v-footer padless>
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>MasterOfNotas</strong>
+      </v-col>
+    </v-footer>
+
   </v-app>
 </template>
 
 <script>
+import MenuQuiz from './components/MenuQuiz.vue'
+// import QuestQuiz from './components/QuestQuiz.vue'
   export default {
-    data: () => ({ drawer: null }),
+    components: {
+        MenuQuiz
+    },
+    data: () => ({ 
+      drawer: true,
+      items: [
+        {
+          text: 'Dashboard',
+          disabled: false,
+          href: '/',
+        },
+        {
+          text: 'Disciplina X',
+          disabled: false,
+          href: 'disciplina',
+        },
+        {
+          text: 'Quiz funções',
+          disabled: true,
+          href: 'breadcrumbs_link_2',
+        },
+      ],
+    }),
   }
 </script>
+<style scoped>
+header {
+  top: 0 !important;
+  left: 0 !important;
+  z-index: 99999 !important;
+}
+nav,
+main {
+  margin-top: 4rem !important;
+}
+nav {
+  max-height: calc(100% - 7rem) !important;
+  border-color: transparent !important;
+  border: 0 !important;
+  position: absolute !important;
+}
+main {
+  padding: 2rem;
+  background-color: #eee;
+}
+footer{
+  background-color: #0038A8 !important;
+  color: #fff !important;
+}
+.breadcrumbs-quiz{
+  padding-left: 2%;
+}
+.card-question{
+  width: 96%;
+  margin: 10px 2%;
+}
+.btns-nav-quiz{
+  width: 96%;
+  margin: 30px 2%;
+}
+.btns-nav-quiz button{
+  width: 48%;
+  font-size: 20px;
+}
+.btns-nav-quiz button:nth-child(1){
+  background-color: #1976D2 !important;
+  margin-right: 2%;
+}
+.btns-nav-quiz button:nth-child(2){
+  background-color: #0038A8 !important;
+  margin-left: 2%;
+}
+</style>
---
+++ src/modules/Quiz/components/QuestQuiz.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-card class="card-question">
+    <v-card-text>
+      <div class="num-label-quest-quiz">Questão 1</div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio quas, amet omnis libero maxime exercitationem consectetur voluptatem dicta molestias ab nulla magni atque! Cupiditate corporis magni molestias illo officia?</p>
+      <v-radio-group v-model="radioGroup">
+        <v-radio
+          v-for="n in 5"
+          :key="n"
+          label="Alternativa"
+          color="blue darken-2"
+          class="blue--text"
+        ></v-radio>
+      </v-radio-group>
+    </v-card-text>
+  </v-card>
+</template>
+<script>
+// import axios from '@/configs/axios'
+export default{
+    data(){
+        return {}
+    },
+    watch:{
+        '$route.params.questionid':function(){
+            this.get_question()
+        },
+    },
+    methods:{
+        get_question(){
+            console.log('E AII')
+        }
+    },
+    created(){
+        this.get_question()
+    }
+
+}
+</script>
+
+<style scoped>
+.num-label-quest-quiz{
+  background-color: #1976D2;
+  color: #fff;
+  font-size: 16px;
+  font-weight: 700;
+  padding: 10px;
+  width: auto;
+  display: table;
+  border-radius: 2px;
+  margin-bottom: 16px;
+  margin-top: 10px;
+}
+.blue--text{
+    color: #1976D2;
+}
+</style>
